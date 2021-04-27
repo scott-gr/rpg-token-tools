1a208fa1dc0e65e3ef9b0582453b5f29f8438f35
Scott Griffin <mscottgriffin@gmail.com>
2021-04-27 16:00:14 -0400

    emotion css layout

---
+++ src/components/UploadForm/UploadForm.jsx
@@ -0,0 +1,25 @@
+// import React from 'react';
+
+// function UploadForm() {
+//   	const [selectedFile, setSelectedFile] = useState();
+// 	const [isFilePicked, setIsFilePicked] = useState(false);
+//   return (
+//     <>
+//       <label htmlFor="imageFile">
+//         Upload an Image
+//         <input
+//           type="file"
+//           id="imageFile"
+//           name="imageFile"
+//           capture="user"
+//           onChange={changeHandler}
+//           accept="image/png, image/jpeg, image/webp"
+//         />{' '}
+//       </label>
+
+//       <button onClick={handleSubmission} type="submit">Submit</button>
+//     </>
+//   );
+// }
+
+// export default UploadForm;
