40b4eca2dd410b9ebecff3f35411fbaf48ca0e4c
Scott Griffin <mscottgriffin@gmail.com>
2021-04-28 16:01:04 -0400

    testing globalstyles with emotion

--- src/components/UploadForm/UploadForm.jsx
+++ src/components/UploadForm/UploadForm.jsx
@@ -1,25 +1,23 @@
-// import React from 'react';
+import React from 'react';
 
-// function UploadForm() {
-//   	const [selectedFile, setSelectedFile] = useState();
-// 	const [isFilePicked, setIsFilePicked] = useState(false);
-//   return (
-//     <>
-//       <label htmlFor="imageFile">
-//         Upload an Image
-//         <input
-//           type="file"
-//           id="imageFile"
-//           name="imageFile"
-//           capture="user"
-//           onChange={changeHandler}
-//           accept="image/png, image/jpeg, image/webp"
-//         />{' '}
-//       </label>
+function UploadForm() {
+  return (
+    <>
+      <label htmlFor="imageFile">
+        Upload an Image
+        <input
+          type="file"
+          id="imageFile"
+          name="imageFile"
+          capture="user"
+          // onChange={changeHandler}
+          accept="image/png, image/jpeg, image/webp"
+        />{' '}
+      </label>
 
-//       <button onClick={handleSubmission} type="submit">Submit</button>
-//     </>
-//   );
-// }
+      <button type="submit">Submit</button>
+    </>
+  );
+}
 
-// export default UploadForm;
+export default UploadForm;
