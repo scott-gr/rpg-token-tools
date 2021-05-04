c6790f71c2b9f44d11265abe1493933600bf80ed
Scott Griffin <mscottgriffin@gmail.com>
2021-05-04 16:13:13 -0400

    image previews after input

--- src/components/UploadForm/UploadForm.jsx
+++ src/components/UploadForm/UploadForm.jsx
@@ -1,12 +1,12 @@
-import React, { useState } from 'react';
+// import React, { useState } from 'react';
+import React, { Component } from 'react';
 import styled from '@emotion/styled';
 
 const ChooseFile = styled.input`
   opacity: 0;
-  cursor: pointer;
 `;
 
-const UploadBtn = styled.button`
+const UploadBtn = styled.div`
   border-radius: 10px;
   transition: all 0.3s cubic-bezier(0.67, 0.17, 0.4, 0.83);
   background-color: var(--appblue);
@@ -15,63 +15,62 @@ const UploadBtn = styled.button`
   display: grid;
   justify-items: center;
   padding: 1rem;
+  /* cursor: pointer; */
 `;
 
-const Success20 = styled.svg`
-  color: var(--appwhite);
-  height: 4rem;
-  width: 4rem;
-  transition: all 0.9s;
-`;
 
-function UploadForm() {
-  const [image, setImage] = useState({ preview: '', raw: '' });
 
-  const changeHandler = (e) => {
-    if (e.target.files.length) {
-      setImage({
-        preview: URL.createObjectURL(e.target.files[0]),
-        raw: e.target.files[0],
+const ImgPreview = styled.img`
+max-height: 300px;
+max-width: 300px;
+object-fit: contain;
+`
+
+class UploadForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      image: null,
+    };
+
+    this.onImageChange = this.onImageChange.bind(this);
+  };
+
+  onImageChange(e) {
+    if (e.target.files && e.target.files[0]) {
+      let img = e.target.files[0];
+      this.setState({
+        image: URL.createObjectURL(img),
       });
     }
   };
-  const uploadHandler = async (e) => {
-    e.preventDefault();
-    const formData = new FormData();
-    formData.append('image', image.raw);
 
-    await fetch('YOUR_URL', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'multipart/form-data',
-      },
-      body: formData,
-    });
-  };
+    render()
+    {
+      return (
+        <>
+        <ImgPreview src={this.state.image}/>
+        <form method="post" enctype="multipart/form-data">
+          <UploadBtn>
+            <label htmlFor="imageFile">Upload an Image </label>
 
-  return (
-    <>
-      <UploadBtn>
-        <Success20 viewBox="0 0 24 24" fill="currentColor">
-          <path d="M20.47 6.62l-7.9-4.44C12.41 2.06 12.21 2 12 2s-.41.06-.57.18l-7.9 4.44c-.32.17-.53.5-.53.88v9c0 .38.21.71.53.88l7.9 4.44c.16.12.36.18.57.18s.41-.06.57-.18l7.9-4.44c.32-.17.53-.5.53-.88v-9c0-.38-.21-.71-.53-.88m-9.02 9.34l-5.14-.03v-1.02s3.43-3.33 3.44-4.34c0-1.24-1.02-1.11-1.02-1.11s-.98.04-1.09 1.25l-1.5.05s.04-2.5 2.69-2.5c2.37 0 2.4 1.78 2.4 2.24 0 1.68-3.08 4.27-3.08 4.27l3.3-.01v1.2m6.05-2.46c0 1.4-1.15 2.55-2.57 2.55-1.43 0-2.57-1.15-2.57-2.55v-2.66c0-1.42 1.14-2.57 2.57-2.57s2.57 1.15 2.57 2.57v2.66M16 10.77v2.76c0 .59-.5 1.07-1.08 1.07-.58 0-1.06-.48-1.06-1.07v-2.76c0-.59.48-1.06 1.06-1.06.58 0 1.08.47 1.08 1.06z" />
-        </Success20>
-        <label htmlFor="uploadBtn">
-          Upload an Image
-          <ChooseFile
-            type="file"
-            id="imageFile"
-            name="imageFile"
-            capture="user"
-            onChange={changeHandler}
-            accept="image/png, image/jpeg, image/webp"
-          />
-        </label>
-      </UploadBtn>
-      <button onClick={uploadHandler} type="submit">
-        Submit
-      </button>
-    </>
-  );
-}
+            <ChooseFile
+              type="file"
+              id="imageFile"
+              name="imageFile"
+              capture="user"
+              onChange={this.onImageChange}
+              accept="image/png, image/jpeg, image/webp"
+            />
+          </UploadBtn>
+          {/* <button onClick={uploadHandler} type="submit">
+          Submit
+        </button> */}
+        </form>
+        </>
+      );
+    }
+  }
+
 
 export default UploadForm;
