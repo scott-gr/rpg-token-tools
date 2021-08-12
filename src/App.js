6a999b49f3156e6e41add6369c806d8698f89b57
Scott Griffin <mscottgriffin@gmail.com>
2021-08-12 16:02:45 -0400

    responsive layout changes, removed unnecessary css classes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,3 @@
-/** @jsxImportSource @emotion/react */
-import { css } from '@emotion/react';
 import React from 'react';
 import styled from '@emotion/styled';
 import UploadImage from './components/UploadImage/UploadImage';
@@ -8,64 +6,38 @@ import UploadImage from './components/UploadImage/UploadImage';
 const Layout = styled.main`
   display: flex;
   flex-flow: column nowrap;
-  height: 100%;
   min-height: 100vh;
-  overflow-x: hidden;
-  place-self: center;
-  margin: 1rem 1rem 0 1rem;
-  row-gap: 0.5rem;
-`;
-
-const PageTitle = styled.h1`
-  text-align: center;
-  margin: 0;
-  padding: 0;
-  flex: 0 1 auto;
-`;
-
-const Intro = styled.section`
-  height: fit-content;
-  flex: 0 1 auto;
-  font-size: 1rem;
-  margin: 0;
-  padding: 0;
-`;
-
-const WipMessage = styled.h2`
-  text-align: center;
-  height: auto;
-  font-size: 1rem;
-  margin: 0;
-  padding: 0;
-  color: var(--appred);
-`;
-
-const HowTo = styled.ol`
-  font-size: 1rem;
-  height: auto;
-  padding: 0 2rem;
+  padding: 0 1rem;
+  & > * {
+    margin-top: 0.5rem;
+    margin-bottom: 0.5rem;
+  }
+  & > :first-child:not(header) {
+    margin-top: 0;
+  }
+  & > :last-child:not(header) {
+    margin-bottom: 0;
+  }
+  & > header > h1 {
+    margin-top: auto;
+    margin-bottom: auto;
+    text-align: center;
+  }
 `;
 
 function App() {
   return (
     <>
       <Layout>
-        <PageTitle>RPG Token Tools</PageTitle>
-        <Intro>
-          <HowTo>
+        <header>
+          <h1>RPG Token Tools</h1>
+          <ol>
             <li>Upload an image</li>
             <li>Edit border, text, and overlay</li>
             <li>Download image</li>
-          </HowTo>
-          <WipMessage>
-            Work in progress. Some features may not be functional.
-          </WipMessage>
-        </Intro>
-        <UploadImage
-          css={css`
-            flex: 1 0 auto;
-          `}
-        />
+          </ol>
+        </header>
+        <UploadImage />
       </Layout>
     </>
   );
